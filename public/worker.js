9ffb210508b06900a82e71386a42ea3d54d40eea
bhar4t <seizedbharat@gmail.com>
2020-04-07 21:20:54 +0530

    worker - cache name changed!

--- public/worker.js
+++ public/worker.js
@@ -1,11 +1,18 @@
-var CACHE_NAME = "pwa-task-manager";
-var urlsToCache = ["/", "/states", "/map"];
+var CACHE_NAME = "covid19v1";
+var urlsToCache = [
+  "/",
+  "/states",
+  "/map",
+  "/error.gif",
+  "/corona-in.png",
+  "android-icon-36x36.png",
+];
 
 // Install a service worker
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       console.log("Opened cache");
       return cache.addAll(urlsToCache);
     })
@@ -13,9 +20,9 @@ self.addEventListener("install", event => {
 });
 
 // Cache and return requests
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       // Cache hit - return response
       if (response) {
         return response;
@@ -26,12 +33,12 @@ self.addEventListener("fetch", event => {
 });
 
 // Update a service worker
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   var cacheWhitelist = ["pwa-task-manager"];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
